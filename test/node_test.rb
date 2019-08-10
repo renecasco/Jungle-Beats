b7208f72c3ef07717451e15ef4a40c9aea594141
Rene Casco <34440390+renecasco@users.noreply.github.com>
2019-08-10 11:27:46 -0600

    adds tests for data and next_node

--- test/node_test.rb
+++ test/node_test.rb
@@ -9,4 +9,14 @@ class NodeTest < Minitest::Test
     node = Node.new("plop")
     assert_instance_of Node, node
   end
+
+  def test_it_has_data
+    node = Node.new("plop")
+    assert_equal "plop", node.data
+  end
+
+  def test_next_node_is_nil
+    node = Node.new("plop")
+    assert_nil node.next_node
+  end
 end
