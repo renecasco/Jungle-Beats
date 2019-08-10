7031007be8f2899f7defad5a48a326be99b15dbd
Rene Casco <34440390+renecasco@users.noreply.github.com>
2019-08-10 13:46:56 -0600

    adds append and tail methods

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,8 +1,17 @@
 class LinkedList
   attr_reader :head
-  
+
   def initialize
     @head = nil
   end
 
+  def append(data)
+    return @head = Node.new(data) if head.nil?
+    tail.next_node = Node.new(data)
+  end
+
+  def tail
+    current_node = @head
+    current_node = current_node.next_node while current_node.next_node
+  end
 end
